176ceadd0eea694cc598b9da8652f7d68ab5b9e9
Clementine <klemstark@gmail.com>
2022-01-28 15:26:55 +0100

    .env added

--- server.js
+++ server.js
@@ -1,42 +1,16 @@
 const express = require("express");
 const server = express();
+require("dotenv").config();
 const morgan = require("morgan");
 const bookRouter = require("./routers/booksRouter");
 const authorRouter = require("./routers/authorsRouter");
 const router = require("./routers/router");
 const api = require("./api/gutendex_api");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const session = require("express-session");
-
-server.use(
-  session({
-    secret: "keyboard cat",
-    resave: true,
-    saveUninitialized: true,
-    cookie: { maxAge: 60000 },
-  })
-);
-
-mongoose.connect("mongodb://localhost/look4books", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 
 server.use(express.static("public"));
 
 server.use(morgan("dev"));
 
-// parse the URL-encoded data with the querystring library
-server.use(bodyParser.urlencoded({ extended: false }));
-
-server.set("trust proxy", 1);
-
-server.use((req, res, next) => {
-  res.locals.message = req.session.message;
-  delete req.session.message;
-  next();
-});
 server.use("/books/", bookRouter);
 server.use("/authors/", authorRouter);
 server.use("/", router);
