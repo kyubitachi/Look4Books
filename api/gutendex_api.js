ff454ebb043866fc0ed16dff64bc4c6a44f64829
Clementine <klemstark@gmail.com>
2022-02-01 18:52:41 +0100

    env +

--- api/gutendex_api.js
+++ api/gutendex_api.js
@@ -1,7 +1,92 @@
-const axios = require("axios");
+// const axios = require("axios");
 
+/*
+const express = require("express");
+const fs = require("fs");
+const dbJsonPath = "./database/db.json";
+const dbJsonRawData = fs.readFileSync(dbJsonPath);
+let dbJson = JSON.parse(dbJsonRawData);
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser());
+
+app.get("/", (req, res) => {
+  res.json({ message: "bonjour" });
+});
+
+app.get("/datas", (req, res) => {
+  res.json(dbJson);
+});
+
+app.get("/datas/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if (isNaN(id) || id > dbJson.length) {
+    res.send("nope");
+  }
+  const data = dbJson.filter((data) => data.id == req.params.id);
+  res.json(data);
+});
+
+app.get("/data/add", (req, res) => {
+  res.render("partials/dataadd");
+});
+app.get("/data/edit/:id", (req, res) => {
+  const { id } = req.params;
+  const data = dbJson.filter((data) => data.id == id)[0];
+  console.log(data);
+  res.render("partials/dataedit", {
+    data: data,
+  });
+});
+
+app.post("/datas", (req, res) => {
+  let { title, authors, bookshelves, image } = req.body;
+  const data = {
+    id: dbJson.length + 1,
+    title,
+    authors: {
+      birth,
+      death,
+    },
+    bookshelves,
+    image,
+  };
+  dbJson.push(data);
+  fs.writeFileSync(dbJsonPath, JSON.stringify(dbJson, null, 4));
+
+  res.redirect("/");
+});
+
+app.post("/datas/:id", (req, res, next) => {
+  let { title, authors, bookshelves, image } = req.body;
+  dbJson = dbJson.map((data) => {
+    if (data.id == req.params.id) {
+      return {
+        ...data,
+        title,
+        authors,
+        bookshelves,
+        image,
+      };
+    } else {
+      return data;
+    }
+  });
+
+  fs.writeFileSync(dbJsonPath, JSON.stringify(dbJson, null, 4));
+  res.redirect("/data");
+});
+*/
+
+/*
+app.listen(3000, () => {
+  console.log(`Listening on port ${3000}`);
+});
+*/
+/*
 axios
-  .get("https://gutendex.com/books/11/")
+  .get("https://gutendex.com/books/11")
   .then((res) => {
     console.log(res.data.title);
     console.log(res.data.authors[0].name);
@@ -9,3 +94,4 @@ axios
   .catch((error) => {
     console.log(error);
   });
+*/
--- server.js
+++ server.js
@@ -1,20 +1,55 @@
 const express = require("express");
+const http = require("http");
 const server = express();
-require("dotenv").config();
 const morgan = require("morgan");
 const bookRouter = require("./routers/booksRouter");
 const authorRouter = require("./routers/authorsRouter");
 const router = require("./routers/router");
 const api = require("./api/gutendex_api");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+
+require("dotenv").config();
+
+const port = process.env.APP_PORT;
+const localhost = process.env.APP_LOCALHOST;
+const secret = process.env.SECRET_APP;
+const dbcredentials = process.env.DB_CREDENTIALS;
+
+mongoose.connect(dbcredentials, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+server.use(
+  session({
+    secret: secret,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 },
+  })
+);
 
 server.use(express.static("public"));
 
 server.use(morgan("dev"));
 
+// parse the URL-encoded data with the querystring library
+server.use(bodyParser.urlencoded({ extended: false }));
+
+server.set("trust proxy", 1);
+
+server.use((req, res, next) => {
+  res.locals.message = req.session.message;
+  delete req.session.message;
+  next();
+});
+
 server.use("/books/", bookRouter);
 server.use("/authors/", authorRouter);
 server.use("/", router);
 
-server.listen(3000, () => {
-  console.log(`Server listen at http://localhost:${3000}`);
+server.listen(port, () => {
+  console.log(`Listening on port http://${localhost}:${port}`);
 });
